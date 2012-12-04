6166b423815878d26980075cc3a3ee05e7a77ec4
Tibs <tibs@tonyibbs.co.uk>
2012-12-04 21:44:37 +0000

    And some Ajax stuff to report on what changes are made to the text

--- server.py
+++ server.py
@@ -7,9 +7,15 @@ class MainHandler(tornado.web.RequestHandler):
         with open('example.html') as fd:
             self.write(fd.read())
 
+class EditorHandler(tornado.web.RequestHandler):
+    def post(self):
+        #self.write(self.get_argument("data"))
+        print self.request.body
+
 application = tornado.web.Application([
     (r"/", MainHandler),
-])
+    (r"/test", EditorHandler),
+], debug=True)  # debug means reload this file when it changes...
 
 if __name__ == "__main__":
     application.listen(8888)
